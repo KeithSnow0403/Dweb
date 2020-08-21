546d12a1d2586c1a27041d7cb517d618cd727e35
KeithSnow <572626298@qq.com>
2020-08-21 19:06:31 +0800

    第十七节（M）
    一对多关系表

--- mysite/myfirstsite/myblog/admin.py
+++ mysite/myfirstsite/myblog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from myblog.models import SiteInfo
+from myblog.models import SiteInfo,Classes,Userinfo
 # Register your models here.
 admin.site.register(SiteInfo) # Django自带，用来看后台
+admin.site.register(Classes)
+admin.site.register(Userinfo)
--- mysite/myfirstsite/myblog/models.py
+++ mysite/myfirstsite/myblog/models.py
@@ -8,3 +8,18 @@ class SiteInfo(models.Model):
         return self.id
     # def __str__(self):
         # return self.title
+
+# 课程分类
+class Classes(models.Model):
+    text = models.CharField(null=True, blank=True,max_length=50)
+    def __str__(self):
+        return self.text
+
+# 用户
+class Userinfo(models.Model):
+    nickname = models.CharField(null=True, blank=True,max_length=50)
+    headImg = models.ImageField(null=True, blank=True, upload_to='userinfo/')
+    belong = models.ForeignKey(Classes, on_delete=models.SET_NULL,
+    related_name="userinfo_classes",null=True,blank=True)
+    def __str__(self):
+        return self.nickname
