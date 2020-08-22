33c1201d6bf21f1c26843754167faa22b4d0d736
KeithSnow <572626298@qq.com>
2020-08-22 16:08:34 +0800

    第二十三节
    数据的序列化

--- mysite/myfirstsite/myblog/api.py
+++ mysite/myfirstsite/myblog/api.py
@@ -1,9 +1,19 @@
 # 类似与view.py的功能
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Classes,Userinfo
+from .tojson import Classes_data,Userinfo_data 
 
 @api_view(['GET', 'POST'])
 def api_test(requset):
-    if requset.method == 'POST':
-        return Response('post')
-    return Response('ok')
+    classes = Classes.objects.all()
+    classes_data = Classes_data(classes,many=True)
+
+    userinfo = Userinfo.objects.all()
+    userinfo_data = Userinfo_data(userinfo,many=True)
+
+    data = {
+        "classes": classes_data.data,
+        "userinfo": userinfo_data.data 
+    }
+    return Response({"data":data})
