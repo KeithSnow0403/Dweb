775c43f6bf3362c403b34ebdc0b5d86268486bde
KeithSnow <572626298@qq.com>
2020-08-22 16:57:23 +0800

    第二十四节
    python字典的数据整理

--- mysite/myfirstsite/myblog/api.py
+++ mysite/myfirstsite/myblog/api.py
@@ -7,13 +7,32 @@ from .tojson import Classes_data,Userinfo_data
 @api_view(['GET', 'POST'])
 def api_test(requset):
     classes = Classes.objects.all()
-    classes_data = Classes_data(classes,many=True)
+    # classes_data = Classes_data(classes,many=True)
 
-    userinfo = Userinfo.objects.all()
-    userinfo_data = Userinfo_data(userinfo,many=True)
+    # userinfo = Userinfo.objects.all()
+    # userinfo_data = Userinfo_data(userinfo,many=True)
+
+    # data = {
+    #     "classes": classes_data.data,
+    #     "userinfo": userinfo_data.data 
+    # }
 
     data = {
-        "classes": classes_data.data,
-        "userinfo": userinfo_data.data 
+        'classes':[]
     }
-    return Response({"data":data})
+    for c in classes:
+        data_item = {
+            "id": c.id,
+            "text": c.text,
+            "userlist": []
+        }
+        userlist = c.userinfo_classes.all() # 外键，详见models.py
+        for user in userlist:
+            user_data = {
+                "id": user.id,
+                "nickName": user.nickname,
+                "headImg": str(user.headImg)
+            }
+            data_item["userlist"].append(user_data)
+        data["classes"].append(data_item)
+    return Response(data)
