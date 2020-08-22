a24e11752291bcda62e8fd3c6c6d92e75371faaa
KeithSnow <572626298@qq.com>
2020-08-22 12:28:45 +0800

    第二十一节
    认识Json和错误处理

--- mysite/myfirstsite/myblog/views.py
+++ mysite/myfirstsite/myblog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,JsonResponse
 from myblog.models import SiteInfo, Classes, Userinfo
 
 # Create your views here.
@@ -32,10 +33,21 @@ def classes(request):
     # 菜单分类
     classes = Classes.objects.all()
     # 当前用户列表
-    choosed_id = request.GET['id']
-    print(choosed_id)
-    choosed = Classes.objects.get(id=choosed_id) # 之前删除过两次，所以从3开始
-    userlist = Userinfo.objects.filter(belong=choosed)
+    try:
+        # 以下为判断班级（id）是否存在，不存在直接甩回主页
+        choosed_id = request.GET['id']
+        print(choosed_id)
+        choosed = Classes.objects.filter(id=choosed_id) # 之前删除过两次，所以从3开始
+        print(choosed)
+    except:
+        return redirect('/')
+
+    # 班级里无对象，则赋值[]
+    if choosed:
+        userlist = Userinfo.objects.filter(belong=choosed[0])
+    else:
+        userlist = []
+    
     data = {
         "siteinfo": siteinfo,
         "classes": classes,
